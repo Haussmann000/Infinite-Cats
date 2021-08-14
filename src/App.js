70c031452e937a52571eef7a23b152603e7db931
r_takaba <r_takaba@satt.jp>
2021-08-14 16:25:35 +0900

    コメント削除

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ const client = createClient(`${API_BACK_KEY}`);
 const query = 'cats';
 
 export default function InfiniteScrollIndex() {
-  const [list, setList] = useState([]);          //表示するデータ
-  const [hasMore, setHasMore] = useState(true);  //再読み込み判定
+  const [list, setList] = useState([]);  
+  const [hasMore, setHasMore] = useState(true);
   const fetchImage = (imageNumber = 5) => {
     client.photos.search({query, page: imageNumber})
     .then((res) => {
       const data = Object.values(res.photos).map(e => e.src.medium)
-      console.log(Object.values(res.photos))
       setList([...list, ...data])
       if (data.length < 1) {
         setHasMore(false);
@@ -24,21 +23,12 @@ export default function InfiniteScrollIndex() {
       console.error(e);
     })
   }
-    //項目を読み込むときのコールバック
     const loadMore = async (page) => {
-      console.log("api loaded");
-
-
         fetchImage(page);
-
-
-      //取得データをリストに追加
     }
 
-  //各スクロール要素
   const items = (
     <ul>
-      {/* {list.map((value) => <li>{value}</li>)} */}
       {list.map((value) => 
       <li>
         <img src={value} className="photos"></img>
@@ -46,14 +36,7 @@ export default function InfiniteScrollIndex() {
       </li>)}
     </ul>);
   
-  //全体のスタイル
-  const root_style = {
-    // marginLeft : "50px",
-    // marginTop : "50px",
-    // listStyle: "none",
-  }
 
-  //ロード中に表示する項目
   const loader =
   <div className="loader" key={0}>
     <img src="https://nekocatgato.up.seesaa.net/image/Corriendo_gato_L.gif" id="loadingImage"></img>
@@ -62,12 +45,12 @@ export default function InfiniteScrollIndex() {
   // http://nekocatgato.seesaa.net/article/135946029.htmlさんの配布画像
 
   return (
-    <div style={root_style} className="container">
+    <div className="container">
       <InfiniteScroll
-        loadMore={loadMore}    //項目を読み込む際に処理するコールバック関数
-        hasMore={hasMore}      //読み込みを行うかどうかの判定
-        loader={loader}>      {/* 読み込み最中に表示する項目 */}
-          {items}             {/* 無限スクロールで表示する項目 */}
+        loadMore={loadMore}
+        hasMore={hasMore}
+        loader={loader}>
+          {items}
       </InfiniteScroll>
     </div>
   )
